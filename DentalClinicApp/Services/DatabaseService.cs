eca7f09dc06bd10aef76a1e05e172a6d1fb38622
Ivan Melgarejo <ivanjaramelgarejo2020@gmail.com>
2025-04-29 17:22:51 -0400

    Agregar archivos de proyecto.

---
+++ DentalClinicApp/Services/DatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicApp.Services
+{
+    internal class DatabaseService
+    {
+    }
+}
